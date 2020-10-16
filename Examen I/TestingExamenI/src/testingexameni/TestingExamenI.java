b18c27afaad0b8b171a52f51c7d48a9fbb43f516
jazmin0226 <jariass@ucenfotec.ac.cr>
2020-10-16 14:05:26 -0600

    Terminado

---
+++ Examen I/TestingExamenI/src/testingexameni/TestingExamenI.java
@@ -0,0 +1,18 @@
+package testingexameni;
+
+/**
+ *
+ * @author Jazmin Arias
+ */
+public class TestingExamenI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MyForm myForm = new MyForm();
+        myForm.setVisible(true);
+    
+    }
+    
+}
